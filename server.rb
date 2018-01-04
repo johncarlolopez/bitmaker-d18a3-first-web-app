82540450b45b5066d9735370943aa347ad0dcbac
John Carlo Lopez <condottiere.jc@gmail.com>
2018-01-04 15:05:49 -0500

    complete exercise

--- server.rb
+++ server.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+get '/' do
+  erb :index
+end
+
 get '/home' do
   erb :index
 end
@@ -8,6 +12,17 @@ get '/portfolio' do
   erb :gallery
 end
 
+get '/gallery' do
+  erb :gallery
+end
+
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about
+end
+
+get '/favourites' do
+  @fav_links = ['https://reddit.com','https://store.steampowered.com','https://youtube.com','https://twitch.tv','https://en.wikipedia.org/wiki/Main_Page']
+  erb :favourites
 end
